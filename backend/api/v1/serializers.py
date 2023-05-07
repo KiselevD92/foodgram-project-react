951d4354165f40d2b47aa75abf5ff4ea35f785dd
KiselevD92 <noproblem2000@yandex.ru>
2023-05-07 13:13:44 +0300

    first review 1.3

--- backend/api/v1/serializers.py
+++ backend/api/v1/serializers.py
@@ -36,7 +36,7 @@ class UserSerializer(serializers.ModelSerializer):
 
 class SetPasswordSerializer(serializers.Serializer):
     new_password = serializers.CharField(style={'input_type': 'password'})
-    current_password = serializers.CharField(style={'input_type": "password'})
+    current_password = serializers.CharField(style={'input_type': 'password'})
 
     def validate(self, obj):
         try:
