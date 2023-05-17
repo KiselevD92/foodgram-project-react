a62c964f7bf94d5e33f6a0567ec1701e3a029148
Dmitry Kiselev <112926383+KiselevD92@users.noreply.github.com>
2023-05-17 19:46:22 +0300

    Update views.py

--- backend/api/v1/views.py
+++ backend/api/v1/views.py
@@ -83,6 +83,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+    pagination_class = None
     permission_classes = (AllowAny,)
 
 
